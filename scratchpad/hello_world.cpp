f43a3ba53681c3c79bd98ad36c8edf4448a453ab
Matthew Zimmer <matthewz@channeleyes.com>
2017-03-04 12:34:22 -0500

    Created a scratchpad directory where I plan to put exploration and discovery code. Added a simply hello_world program in there for POC.

---
+++ scratchpad/hello_world.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Matthew Zimmer on 3/4/17.
+//
+
+#include "hello_world.h"
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ scratchpad/hello_world.h
@@ -0,0 +1,9 @@
+//
+// Created by Matthew Zimmer on 3/4/17.
+//
+
+#ifndef TERM2_NOTES_HELLO_WORLD_H
+#define TERM2_NOTES_HELLO_WORLD_H
+#include <iostream>
+using namespace std;
+#endif //TERM2_NOTES_HELLO_WORLD_H
